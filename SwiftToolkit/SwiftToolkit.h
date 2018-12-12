1a5f0f832ca3a36eb1e2249d5d3abe55de29f4c8
Avery Pierce <aapierce0@gmail.com>
2018-12-12 11:49:21 -0600

    Create Xcode project

---
+++ SwiftToolkit/SwiftToolkit.h
@@ -0,0 +1,19 @@
+//
+//  SwiftToolkit.h
+//  SwiftToolkit
+//
+//  Created by Avery Pierce on 12/12/18.
+//  Copyright © 2018 Avery Pierce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftToolkit.
+FOUNDATION_EXPORT double SwiftToolkitVersionNumber;
+
+//! Project version string for SwiftToolkit.
+FOUNDATION_EXPORT const unsigned char SwiftToolkitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftToolkit/PublicHeader.h>
+
+
