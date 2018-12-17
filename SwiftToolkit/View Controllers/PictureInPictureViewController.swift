de2ac0fc8d8d59ff1f1d768bbdeb52d3d9087c7c
Avery Pierce <aapierce0@gmail.com>
2018-12-17 15:09:27 -0600

    Adds a Picture in Picture view controller

---
+++ SwiftToolkit/View Controllers/PictureInPictureViewController.swift
@@ -0,0 +1,107 @@
+//
+//  PictureInPictureViewController.swift
+//  SwiftToolkit
+//
+//  Created by Avery Pierce on 12/17/18.
+//  Copyright © 2018 Avery Pierce. All rights reserved.
+//
+
+import UIKit
+
+public class PictureInPictureViewController: UIViewController {
+    
+    private(set) public var primaryViewController: UIViewController!
+    private let pictureInPictureContainerViewController = ContainerViewController()
+    public var pictureInPictureViewController: UIViewController? {
+        return pictureInPictureContainerViewController.contentViewController
+    }
+    
+    private weak var pictureInPictureWrapperView: UIView!
+    
+    convenience init(primaryViewController: UIViewController) {
+        self.init()
+        setPrimary(primaryViewController)
+    }
+    
+    override public func loadView() {
+        view = UIView()
+        
+        loadPrimaryViewController()
+        loadPictureInPictureWrapperView()
+        loadPictureInPictureContainerViewController()
+    }
+    
+    private func loadPrimaryViewController() {
+        guard let primaryViewController = primaryViewController else {
+            fatalError("Attempted to load PictureInPictureViewController without primaryViewController")
+        }
+        
+        addChild(primaryViewController)
+        view.addSubview(primaryViewController.view)
+        activateLayoutConstraintsForPrimaryViewController()
+        primaryViewController.didMove(toParent: self)
+    }
+    
+    private func activateLayoutConstraintsForPrimaryViewController() {
+        guard let contentView = primaryViewController.view else { return }
+        
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        contentView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        contentView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+    
+    private func loadPictureInPictureWrapperView() {
+        let wrapperView = UIView()
+        wrapperView.layer.cornerRadius = 4.0
+        view.addSubview(wrapperView)
+        pictureInPictureWrapperView = wrapperView
+        activateLayoutConstraintsForPictureInPictureWrapperView()
+    }
+    
+    private func activateLayoutConstraintsForPictureInPictureWrapperView() {
+        let pipView = pictureInPictureWrapperView!
+        
+        pipView.translatesAutoresizingMaskIntoConstraints = false
+        pipView.topAnchor.constraint(equalTo: view.topAnchor, constant: 8.0).isActive = true
+        pipView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8.0).isActive = true
+        pipView.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        pipView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+    }
+    
+    private func loadPictureInPictureContainerViewController() {
+        pictureInPictureWrapperView.addSubview(pictureInPictureContainerViewController.view)
+        activateLayoutConstraintsForPictureInPictureView()
+    }
+    
+    private func activateLayoutConstraintsForPictureInPictureView() {
+        let wrapperView = pictureInPictureWrapperView!
+        let containerView = pictureInPictureContainerViewController.view!
+        
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.topAnchor.constraint(equalTo: wrapperView.topAnchor, constant: 4.0).isActive = true
+        containerView.leadingAnchor.constraint(equalTo: wrapperView.leadingAnchor, constant: 4.0).isActive = true
+        containerView.trailingAnchor.constraint(equalTo: wrapperView.trailingAnchor, constant: -4.0).isActive = true
+        containerView.bottomAnchor.constraint(equalTo: wrapperView.bottomAnchor, constant: -4.0).isActive = true
+        containerView.setNeedsLayout()
+    }
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    public func setPrimary(_ viewController: UIViewController) {
+        if isViewLoaded {
+            fatalError("Attempted to set primaryViewController after it was already loaded")
+        } else {
+            primaryViewController = viewController
+        }
+    }
+    
+    public func setPictureInPicture(_ viewController: UIViewController) {
+        pictureInPictureContainerViewController.setContent(viewController)
+    }
+}
---
+++ SwiftToolkitTests/View Controllers/PictureInPictureViewControllerTests.swift
@@ -0,0 +1,91 @@
+//
+//  PictureInPictureViewControllerTests.swift
+//  SwiftToolkitTests
+//
+//  Created by Avery Pierce on 12/17/18.
+//  Copyright © 2018 Avery Pierce. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftToolkit
+
+class PictureInPictureViewControllerTests: XCTestCase {
+    
+    var viewController: PictureInPictureViewController!
+
+    override func setUp() {
+        viewController = PictureInPictureViewController()
+    }
+
+    override func tearDown() {
+        viewController = nil
+    }
+
+    func testInitWithPrimaryViewController() {
+        let mockViewController = MockViewController()
+        viewController = PictureInPictureViewController(primaryViewController: mockViewController)
+        XCTAssert(viewController.primaryViewController === mockViewController)
+    }
+    
+    func testViewLoadsWithPrimaryContent() {
+        let mockViewController = MockViewController()
+        viewController.setPrimary(mockViewController)
+        loadView()
+        XCTAssert(viewController.primaryViewController === mockViewController)
+        XCTAssertTrue(mockViewController.isViewLoaded)
+    }
+    
+    func testPrimaryContentResizes() {
+        let mockViewController = MockViewController()
+        viewController.setPrimary(mockViewController)
+        loadView()
+        
+        resizeView(CGSize(width: 100, height: 100))
+        viewController.view.layoutIfNeeded()
+        XCTAssertEqual(mockViewController.view.frame, CGRect(x: 0, y: 0, width: 100, height: 100))
+        
+        resizeView(CGSize(width: 200, height: 200))
+        viewController.view.layoutIfNeeded()
+        XCTAssertEqual(mockViewController.view.frame, CGRect(x: 0, y: 0, width: 200, height: 200))
+    }
+    
+    func testPictureInPictureAppears() {
+        let primaryViewController = MockViewController()
+        viewController.setPrimary(primaryViewController)
+        let pipViewController = MockViewController()
+        viewController.setPictureInPicture(pipViewController)
+        loadView()
+        resizeView(CGSize(width: 400, height: 800))
+        viewController.view.layoutIfNeeded()
+        
+        XCTAssertEqual(viewController.view.subviews.count, 2)
+        let wrapperView = viewController.view.subviews[1]
+        XCTAssertEqual(wrapperView.frame, CGRect(x: 312, y: 8, width: 80, height: 120))
+        
+        let contentView = pipViewController.view!
+        XCTAssertEqual(contentView.frame, CGRect(x: 0.0, y: 0.0, width: 72, height: 112))
+        
+        let containerView = contentView.superview!
+        XCTAssertEqual(containerView.frame, CGRect(x: 4.0, y: 4.0, width: 72, height: 112))
+    }
+    
+    func testPictureInPictureAfterLoad() {
+        let primaryViewController = MockViewController()
+        viewController.setPrimary(primaryViewController)
+        
+        loadView()
+        let mockViewController = MockViewController()
+        viewController.setPictureInPicture(mockViewController)
+        XCTAssert(viewController.pictureInPictureViewController === mockViewController)
+    }
+    
+    @discardableResult func loadView() -> UIView {
+        return viewController.view
+    }
+    
+    func resizeView(_ size: CGSize) {
+        viewController.view.frame.size = size
+        viewController.view.setNeedsLayout()
+    }
+
+}
